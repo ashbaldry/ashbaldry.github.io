a3125067654a4f6d7209313c4af14a6487db6b8d
Ashley Baldry <arbaldry91@gmail.com>
2021-09-27 21:11:55 +0100

    Update crossstitch.js

--- assets/js/crossstitch.js
+++ assets/js/crossstitch.js
@@ -1,10 +1,1 @@
-$('img').modal({
-    title: 'Important Notice',
-    class: 'mini',
-    closeIcon: true,
-    content: 'You will be logged out in 5 Minutes',
-    actions: [{
-      text: 'Alright, got it',
-      class: 'green'
-    }]
-}).modal('show');
+
